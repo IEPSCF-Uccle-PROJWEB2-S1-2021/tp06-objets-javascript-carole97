acfc78c86815563fc6ace09f87973cd701dc3c93
52060 <52060@etu.he2b.be git config --globla user.name carole>
2020-10-19 20:44:35 +0200

    exercice2

---
+++ classe.js
@@ -0,0 +1,20 @@
+let Shape = class {
+    constructor(name, sides, sideLength) {
+      this.name = name;
+      this.sides = sides;
+      this.sideLength = sideLength;
+    }
+    calcPerimeter() {
+      let perimetre = 0;
+      for (let i = 0; i < this.sides; ++i) {
+        perimetre = perimetre + this.sideLength;
+      }
+      return perimetre ;
+    }
+  };
+  let square = new Shape ('square',4,5);
+  
+  console.log(square.calcPerimeter());
+  
+  let triangle = new Shape('Triangle',3,3);
+  console.log(triangle.calcPerimeter());
